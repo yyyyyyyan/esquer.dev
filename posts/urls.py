a991bfcbe15a76924d26b9b1c7756162aeb4d6d1
yanorestes <yanorestes@hotmail.com>
2019-12-04 20:11:17 -0300

    Added privacy policy and terms of use

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
+    path('termos-de-uso', TemplateView.as_view(template_name='terms.html'), name='terms'),
+    path('politica-de-privacidade', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
+    path('contato', TemplateView.as_view(template_name='contact.html'), name='contact'),
     path('', views.PostList.as_view(), name='index'),
     path('posts/<slug:slug>', views.PostDetail.as_view(), name='detail')
 ]
