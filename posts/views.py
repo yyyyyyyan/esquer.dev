e6ca016af188e9d27adf5a619eae733667a49443
yanorestes <yanorestes@hotmail.com>
2019-12-11 21:36:38 -0300

    Changed style of links. Added tag to titles on tag page.

--- posts/views.py
+++ posts/views.py
@@ -24,5 +24,10 @@ class PostListByTag(PostList):
     def get_queryset(self):
         queryset = self.model.objects.filter(keywords__icontains=self.kwargs['keyword']).order_by(self.ordering)
         return queryset
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(PostListByTag, self).get_context_data(*args, **kwargs)
+        context['keyword'] = self.kwargs['keyword']
+        return context
 
 # TODO: paginação
