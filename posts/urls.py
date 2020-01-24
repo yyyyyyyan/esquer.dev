5d11c04fea3575d9309d5829826b3ea544092b0b
yanorestes <yanorestes@hotmail.com>
2020-01-23 22:39:20 -0300

    Fixed urls with slash

--- posts/urls.py
+++ posts/urls.py
@@ -4,13 +4,13 @@ from . import views
 from . import feeds
 
 urlpatterns = [
-    path('termos-de-uso', TemplateView.as_view(template_name='terms.html'), name='terms'),
-    path('politica-de-privacidade', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
-    path('sobre', TemplateView.as_view(template_name='about.html'), name='about'),
-    path('contato', TemplateView.as_view(template_name='contact.html'), name='contact'),
-    path('posts', views.PostList.as_view(), name='post_list'),
+    path('termos-de-uso/', TemplateView.as_view(template_name='terms.html'), name='terms'),
+    path('politica-de-privacidade/', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
+    path('sobre/', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contato/', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    path('posts/', views.PostList.as_view(), name='post_list'),
     path('', RedirectView.as_view(pattern_name='post_list', permanent=True), name='index'),
-    path('posts/feed', feeds.PostsFeed(), name='posts_rss'),
-    path('posts/<slug:slug>', views.PostDetail.as_view(), name='post_detail'),
-    path('tags/<keyword>', views.PostListByTag.as_view(), name='search_keyword'),
+    path('posts/feed/', feeds.PostsFeed(), name='posts_rss'),
+    path('posts/<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('tags/<keyword>/', views.PostListByTag.as_view(), name='search_keyword'),
 ]
