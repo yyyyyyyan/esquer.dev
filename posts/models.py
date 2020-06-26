5ec5fe5a178a02b8bf8fbbf2bc019352f4f62227
yyyyyyyan <yanorestes@hotmail.com>
2020-06-26 18:07:48 -0300

    Fix keywords flowing out of .post-keywords div

--- posts/models.py
+++ posts/models.py
@@ -15,6 +15,7 @@ class Post(models.Model):
     has_code = models.BooleanField(editable=False)
 
     def save(self, *args, **kwargs):
+        self.keywords = ', '.join({keyword.strip() for keyword in self.keywords.split(',')})
         self.lock_key = self.lock_key if self.lock_key else uuid4().hex
         self.readtime = readtime.of_markdown(self.markdown, wpm=250).minutes
         self.has_code = "```" in self.markdown
