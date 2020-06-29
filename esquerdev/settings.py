8147ce0764b634bb8b8941b4a860fe949a765ae9
yyyyyyyan <yanorestes@hotmail.com>
2020-06-29 03:20:50 -0300

    Created middleware to get real ip on REMOTE_ADDR

--- esquerdev/settings.py
+++ esquerdev/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'middleware.remote_addr.RemoteAddrMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
