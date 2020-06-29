1590f696ad54638a24c827042cf549fa8f03ad1b
yyyyyyyan <yanorestes@hotmail.com>
2020-06-29 17:37:10 -0300

    Fixed RemoteAddrMiddleware

--- middleware/remote_addr.py
+++ middleware/remote_addr.py
@@ -2,5 +2,9 @@ class RemoteAddrMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
+    def __call__(self, request):
+        response = self.get_response(request)
+        return response
+
     def process_request(self, request):
         request.META["REMOTE_ADDR"] = request.META.get("HTTP_X_REAL_IP", "127.0.0.1")
