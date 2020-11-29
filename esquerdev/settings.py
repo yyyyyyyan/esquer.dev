114f339a690ec49cfb06e07e42b807291313a138
yyyyyyyan <contact@yyyyyyyan.tech>
2020-11-29 13:12:41 -0300

    Replace os.path with pathlib.Path.

--- esquerdev/settings.py
+++ esquerdev/settings.py
@@ -10,10 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import os
+from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'esquerdev.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,7 +107,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
- os.path.join(BASE_DIR, "static"),
+ BASE_DIR / "static",
 ]
 
 SITE_ID = 1
--- tools/views.py
+++ tools/views.py
@@ -5,7 +5,7 @@ from ratelimit.decorators import ratelimit
 from ratelimit import UNSAFE as UNSAFE_METHODS
 from .forms import MetadataRemovalForm
 from uuid import uuid4
-import os
+from pathlib import Path
 import subprocess
 
 
@@ -16,17 +16,19 @@ def remove_metadata(request):
         form = MetadataRemovalForm(request.POST, request.FILES)
         if form.is_valid():
             file_to_clean = request.FILES["file_to_clean"]
-            save_as = uuid4().hex + file_to_clean.name
-            filename, extension = os.path.splitext(file_to_clean.name)
-            tmp_file_path = os.path.join(settings.TEMP_SAVE_FOLDER, save_as)
-            with open(tmp_file_path, "wb+") as tmp_file:
+            filename = Path(file_to_clean.name)
+            save_as = filename.with_name(uuid4().hex + filename.name)
+            exts = "".join(filename.suffixes)
+            cleaned_filename = filename.with_name(filename.name.replace(exts, ".cleaned" + exts))
+            tmp_file_path = settings.TEMP_SAVE_FOLDER / save_as
+            with tmp_file_path.open("wb+") as tmp_file:
                 for chunk in file_to_clean.chunks():
                     tmp_file.write(chunk)
             subprocess.run(["mat2", "--inplace", tmp_file_path])
             try:
-                return FileResponse(open(tmp_file_path, "rb"), as_attachment=True, filename=filename+".cleaned"+extension)
+                return FileResponse(tmp_file_path.open("rb"), as_attachment=True, filename=cleaned_filename.name)
             finally:
-                os.remove(tmp_file_path)
+                tmp_file_path.unlink()
     else:
         form = MetadataRemovalForm()
     return render(request, "tools/remove_metadata.html", {"form": form})
