47d888cebf7fec9214c0299e884130855bea59d4
yyyyyyyan <contact@yyyyyyyan.tech>
2021-04-03 18:45:48 -0300

    Add syntax highlighting.

--- posts/templatetags/markdown.py
+++ posts/templatetags/markdown.py
@@ -2,7 +2,11 @@ from django import template
 from markdown import Markdown
 
 register = template.Library()
-md = Markdown(extensions=["fenced_code", "toc"])
+md = Markdown(
+    extensions=["fenced_code", "codehilite", "toc"],
+    extension_configs={"codehilite": {"linenums": False, "guess_lang": False}},
+    output_format="html5",
+)
 
 
 @register.filter()
